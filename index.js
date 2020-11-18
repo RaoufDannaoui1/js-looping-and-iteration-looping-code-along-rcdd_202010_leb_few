c642ea3a5917d97013a0a1e6fd527865a5095016
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-11-18 09:14:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const names = ["Lisa", "Kaitlin", "Jan"];
 
   writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
 
-function writeCards(names,event)
-{
-  for(let i=0; i<names.length; i++)
-console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-
-
-  return names;
-}
+  function writeCards( names, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
