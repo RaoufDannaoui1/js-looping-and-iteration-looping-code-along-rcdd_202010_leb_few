8735b90c37eb467948ee9bd9da41eb8f90d71585
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-11-18 08:58:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // Code your solutions in this file
 const names = ["Ada", "Brendan", "Ali"];
-
-  writeCards(names);
+for(let i = 0; i<names.length; i++)
+  writeCards(names[i]);
 
 function writeCards(names)
 {
-for(let i = 0; i<names.length; i++)
-  {
-    console.log(`Thank you, ${names[i]}, for the wonderful birthday gift!`);
-  }
+nsole.log(`Thank you, ${names[i]}, for the wonderful birthday gift!`);
+
 
   return names;
 }
