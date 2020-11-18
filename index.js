8b11d15fd6565ce30272d7ad85fbfdc12ebd5d17
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-11-18 09:15:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const names = ["Lisa", "Kaitlin", "Jan"];
 
   function writeCards( names, event ) {
     let thankYouCards = []
-    for ( let i = 0; i < namesArray.length; i++ ) {
+    for ( let i = 0; i < names.length; i++ ) {
       thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
     }
     return thankYouCards;
